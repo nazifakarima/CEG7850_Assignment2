7a8d2624a7bd2abb269a2c39f45bc2dc3d80fc17
nazifakarima <nazifakarima@gmail.com>
2015-03-04 19:29:43 -0500

    1 and 2 done

--- OrigDecisionTree/src/ID3Tree.java
+++ OrigDecisionTree/src/ID3Tree.java
@@ -6,187 +6,260 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class ID3Tree {
-	public static ArrayList<Boolean[]> testData = new ArrayList<Boolean[]>();
-	public static int[] percentage = { 10, 25, 50, 75, 100 };
 
-	public static ArrayList<Boolean[]> readFile(String filename)
-			throws Exception {
+	public class UnmodifiedDataUnmodifiedTree {
+		public ArrayList<Boolean[]> testData = new ArrayList<Boolean[]>();
+		public int[] percentage = { 10, 25, 50, 75, 100 };
 
-		ArrayList<Boolean[]> data = new ArrayList<>();
-		Scanner input = new Scanner(new File(filename));
+		public UnmodifiedDataUnmodifiedTree() {
 
-		while (input.hasNext()) {
-			String line = input.nextLine();
-			String[] tokens = line.split("[,]");
+			try {
+				testData = readFile("./test.txt");
+				ArrayList<Boolean[]> trainingData = readFile("./training.txt");
+				ArrayList<Boolean> actualClassValue = new ArrayList<Boolean>();
+				for (int i = 0; i < trainingData.size(); i++) {
+					Boolean[] record = trainingData.get(i);
+					actualClassValue.add(record[record.length - 1]);
+				}
+				for (int i = 0; i < percentage.length; i++) {
+					ArrayList<Boolean[]> modifiedTestData = generateTestDataLists(percentage[i]);// 20,50,80
+					System.out.println("Unmodified dataset size: "
+							+ modifiedTestData.size());
+					HashSet<Integer> attributes = getAttributeList(modifiedTestData);
+					Node root = buildTree(modifiedTestData, attributes);
+					traverseTree(root, 0, "");
+					// trees.add(root);
+					ArrayList<Boolean> derivedClassValue = new ArrayList<Boolean>();
+					StringBuilder toBeWritten = new StringBuilder();
+					for (int j = 0; j < trainingData.size(); j++) {
+						Boolean[] record = trainingData.get(j);
+						derivedClassValue.add(classify(root, record));
+						toBeWritten.append(derivedClassValue.get(j));
+						toBeWritten
+								.append(System.getProperty("line.separator"));
+					}
+					String filename = "unmodified_data_output_" + percentage[i]
+							+ "_percent";
+					writeToFile(toBeWritten, filename);
+					Double meanSquareError = compareResults(actualClassValue,
+							derivedClassValue);
+					System.out.println(meanSquareError);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
-			Boolean[] record = new Boolean[tokens.length];
-			for (int i = 0; i < tokens.length; i++) {
-				if (tokens[i].trim().equals("1")) {
-					record[i] = true;
-				} else {
-					record[i] = false;
+		public ArrayList<Boolean[]> readFile(String filename) throws Exception {
+
+			ArrayList<Boolean[]> data = new ArrayList<>();
+			Scanner input = new Scanner(new File(filename));
+
+			while (input.hasNext()) {
+				String line = input.nextLine();
+				String[] tokens = line.split("[,]");
+
+				Boolean[] record = new Boolean[tokens.length];
+				for (int i = 0; i < tokens.length; i++) {
+					if (tokens[i].trim().equals("1")) {
+						record[i] = true;
+					} else {
+						record[i] = false;
+					}
+				}
+				data.add(record);
+			}
+
+			input.close();
+
+			return data;
+		}
+
+		private ArrayList<Boolean[]> generateTestDataLists(int percentage) {
+
+			ArrayList<Boolean[]> data = new ArrayList<Boolean[]>();
+			int size = testData.size();
+			int newSize = (int) ((size * percentage) / 100);
+			// System.out.println("for 50% test data full data" +
+			// testData.size()+
+			// newSize);
+
+			for (int i = 0; i < newSize; i++) {
+				Boolean[] record = testData.get(i);
+				// System.out.println("Building half tree" +
+				// Arrays.toString(record));
+				data.add(record);
+			}
+			return data;
+		}
+
+		private HashSet<Integer> getAttributeList(
+				ArrayList<Boolean[]> modifiedTestData) {
+			HashSet<Integer> attributes;
+			attributes = new HashSet<>();
+			for (int i = 0; i < modifiedTestData.get(0).length - 1; i++) {
+				attributes.add(i);
+			}
+			return attributes;
+		}
+
+		public ArrayList<Boolean[]> getTrueRows(ArrayList<Boolean[]> data,
+				int attribute) {// rows in the remaining database, on which this
+								// specific attribute=true
+			ArrayList<Boolean[]> trueRows = new ArrayList<>();
+
+			for (Boolean[] row : data) {
+				if (row[attribute]) {
+					trueRows.add(row);
 				}
 			}
-			data.add(record);
+
+			return trueRows;
 		}
 
-		input.close();
+		public ArrayList<Boolean[]> getFalseRows(ArrayList<Boolean[]> data,
+				int attribute) {
+			ArrayList<Boolean[]> falseRows = new ArrayList<>();
 
-		return data;
-	}
-
-	public static ArrayList<Boolean[]> getTrueRows(ArrayList<Boolean[]> data,
-			int attribute) {// rows in the remaining database, on which this
-							// specific attribute=true
-		ArrayList<Boolean[]> trueRows = new ArrayList<>();
-
-		for (Boolean[] row : data) {
-			if (row[attribute]) {
-				trueRows.add(row);
+			for (Boolean[] row : data) {
+				if (!row[attribute]) {
+					falseRows.add(row);
+				}
 			}
+
+			return falseRows;
 		}
 
-		return trueRows;
-	}
+		public double findGain(ArrayList<Boolean[]> data, int attribute) {
 
-	public static ArrayList<Boolean[]> getFalseRows(ArrayList<Boolean[]> data,
-			int attribute) {
-		ArrayList<Boolean[]> falseRows = new ArrayList<>();
+			ArrayList<Boolean[]> trueRows = getTrueRows(data, attribute);
+			ArrayList<Boolean[]> falseRows = getFalseRows(data, attribute);
 
-		for (Boolean[] row : data) {
-			if (!row[attribute]) {
-				falseRows.add(row);
+			double gain = entropy(data)
+					- ((trueRows.size() / (double) data.size())
+							* entropy(trueRows) + (falseRows.size() / (double) data
+							.size()) * entropy(falseRows));
+
+			if (Double.isNaN(gain))
+				return 0.0;
+			else
+				return gain;
+		}
+
+		public int selectAttribute(ArrayList<Boolean[]> data,
+				HashSet<Integer> attributes) {
+			double maxGain = -Double.MIN_VALUE;
+			int attribute = -1;
+
+			for (Integer i : attributes) {
+				double gain = findGain(data, i);
+				if (gain > maxGain) {
+					maxGain = gain;
+					attribute = i;
+				}
 			}
+
+			return attribute;
 		}
 
-		return falseRows;
-	}
+		public Node buildTree(ArrayList<Boolean[]> data,
+				HashSet<Integer> attributes) {
+			Node node = new Node();
 
-	public static boolean allOneClass(ArrayList<Boolean[]> data) {
-		return (numTrue(data) == data.size() || numFalse(data) == data.size());
-	}
-
-	public static boolean majorityClass(ArrayList<Boolean[]> data) {
-		return numTrue(data) > numFalse(data);
-	}
-
-	public static int numTrue(ArrayList<Boolean[]> data) {
-		int count = 0;
-		for (Boolean[] row : data) {
-			if (row[row.length - 1]) {
-				count++;
+			if (allOneClass(data)) {
+				node.label = majorityClass(data);
+				return node;
 			}
-		}
-		return count;
-	}
 
-	public static int numFalse(ArrayList<Boolean[]> data) {
-		int count = 0;
-		for (Boolean[] row : data) {
-			if (!row[row.length - 1]) {
-				count++;
+			if (attributes.size() == 0) { // no more attributes to split on
+				node.label = majorityClass(data);
+				return node;
 			}
-		}
-		return count;
-	}
 
-	public static double entropy(ArrayList<Boolean[]> data) {
+			node.attribute = selectAttribute(data, attributes);
+			attributes.remove(node.attribute);
 
-		double Qtrue = numTrue(data) / (double) data.size();
-		double Qfalse = numFalse(data) / (double) data.size();
-
-		double entropy = 0;
-
-		if (Qtrue != 0 && !Double.isNaN(Qtrue)) {
-			entropy += Qtrue * (Math.log(Qtrue) / Math.log(2));
-		}
-
-		if (Qfalse != 0 && !Double.isNaN(Qfalse)) {
-			entropy += Qfalse * (Math.log(Qfalse) / Math.log(2));
-		}
-
-		return -entropy;
-	}
-
-	public static double findGain(ArrayList<Boolean[]> data, int attribute) {
-
-		ArrayList<Boolean[]> trueRows = getTrueRows(data, attribute);
-		ArrayList<Boolean[]> falseRows = getFalseRows(data, attribute);
-
-		double gain = entropy(data)
-				- ((trueRows.size() / (double) data.size()) * entropy(trueRows) + (falseRows
-						.size() / (double) data.size()) * entropy(falseRows));
-
-		if (Double.isNaN(gain))
-			return 0.0;
-		else
-			return gain;
-	}
-
-	public static int selectAttribute(ArrayList<Boolean[]> data,
-			HashSet<Integer> attributes) {
-		double maxGain = -Double.MIN_VALUE;
-		int attribute = -1;
-
-		for (Integer i : attributes) {
-			double gain = findGain(data, i);
-			if (gain > maxGain) {
-				maxGain = gain;
-				attribute = i;
-			}
-		}
-
-		return attribute;
-	}
-
-	public static Node buildTree(ArrayList<Boolean[]> data,
-			HashSet<Integer> attributes) {
-		Node node = new Node();
-
-		if (allOneClass(data)) {
-			node.label = majorityClass(data);
-			return node;
-		}
-
-		if (attributes.size() == 0) { // no more attributes to split on
-			node.label = majorityClass(data);
-			return node;
-		}
-
-		node.attribute = selectAttribute(data, attributes);
-		attributes.remove(node.attribute);
-
-		ArrayList<Boolean[]> trueRows = getTrueRows(data, node.attribute);
-		if (trueRows.size() == 0) {
-			node.trueChild = new Node();
-			node.trueChild.label = majorityClass(data);
-		} else {
-			node.trueChild = buildTree(trueRows, attributes);
-		}
-
-		ArrayList<Boolean[]> falseRows = getFalseRows(data, node.attribute);
-		if (falseRows.size() == 0) {
-			node.falseChild = new Node();
-			node.falseChild.label = majorityClass(data);
-		} else {
-			node.falseChild = buildTree(falseRows, attributes);
-		}
-
-		return node;
-	}
-
-	public static boolean classify(Node node, Boolean[] row) {
-		if (node.label != null) {
-			return node.label;
-		} else {
-			boolean direction = row[node.attribute];
-			if (direction) {
-				return classify(node.trueChild, row);
+			ArrayList<Boolean[]> trueRows = getTrueRows(data, node.attribute);
+			if (trueRows.size() == 0) {
+				node.trueChild = new Node();
+				node.trueChild.label = majorityClass(data);
 			} else {
-				return classify(node.falseChild, row);
+				node.trueChild = buildTree(trueRows, attributes);
+			}
+
+			ArrayList<Boolean[]> falseRows = getFalseRows(data, node.attribute);
+			if (falseRows.size() == 0) {
+				node.falseChild = new Node();
+				node.falseChild.label = majorityClass(data);
+			} else {
+				node.falseChild = buildTree(falseRows, attributes);
+			}
+
+			return node;
+		}
+
+		public boolean allOneClass(ArrayList<Boolean[]> data) {
+			return (numTrue(data) == data.size() || numFalse(data) == data.size());
+		}
+
+		public boolean majorityClass(ArrayList<Boolean[]> data) {
+			return numTrue(data) > numFalse(data);
+		}
+
+		public int numTrue(ArrayList<Boolean[]> data) {
+			int count = 0;
+			for (Boolean[] row : data) {
+				if (row[row.length - 1]) {
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public int numFalse(ArrayList<Boolean[]> data) {
+			int count = 0;
+			for (Boolean[] row : data) {
+				if (!row[row.length - 1]) {
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public double entropy(ArrayList<Boolean[]> data) {
+
+			double Qtrue = numTrue(data) / (double) data.size();
+			double Qfalse = numFalse(data) / (double) data.size();
+
+			double entropy = 0;
+
+			if (Qtrue != 0 && !Double.isNaN(Qtrue)) {
+				entropy += Qtrue * (Math.log(Qtrue) / Math.log(2));
+			}
+
+			if (Qfalse != 0 && !Double.isNaN(Qfalse)) {
+				entropy += Qfalse * (Math.log(Qfalse) / Math.log(2));
+			}
+
+			return -entropy;
+		}
+
+
+		public boolean classify(Node node, Boolean[] row) {
+			if (node.label != null) {
+				return node.label;
+			} else {
+				boolean direction = row[node.attribute];
+				if (direction) {
+					return classify(node.trueChild, row);
+				} else {
+					return classify(node.falseChild, row);
+				}
 			}
 		}
 	}
+
 
 	public static void traverseTree(Node node, int tab, String dir) {
 		if (node.label != null) {
@@ -205,45 +278,15 @@ public class ID3Tree {
 	}
 
 	public static void main(String[] args) throws Exception {
-
-		testData = readFile("./test.txt");
-		ArrayList<Boolean[]> trainingData = readFile("./training.txt");
-		ArrayList<Boolean> actualClassValue = new ArrayList<Boolean>();
-		for (int i = 0; i < trainingData.size(); i++) {
-			Boolean[] record = trainingData.get(i);
-			actualClassValue.add(record[record.length - 1]);
-		}
-
-		for (int i = 0; i < percentage.length; i++) {
-			ArrayList<Boolean[]> modifiedTestData = generateTestDataLists(percentage[i]);// 20,50,80
-			System.out.println("Unmodified dataset size: "
-					+ modifiedTestData.size());
-			HashSet<Integer> attributes = getAttributeList(modifiedTestData);
-			Node root = buildTree(modifiedTestData, attributes);
-			traverseTree(root, 0, "");
-			// trees.add(root);
-			ArrayList<Boolean> derivedClassValue = new ArrayList<Boolean>();
-			StringBuilder toBeWritten = new StringBuilder();
-			for (int j = 0; j < trainingData.size(); j++) {
-				Boolean[] record = trainingData.get(j);
-				derivedClassValue.add(classify(root, record));
-				toBeWritten.append(derivedClassValue.get(j));
-				toBeWritten.append(System.getProperty("line.separator"));
-			}
-			String filename = "unmodified_data_output_" + percentage[i]
-					+ "_percent";
-			writeToFile(toBeWritten, filename);
-			Double meanSquareError = compareResults(actualClassValue,
-					derivedClassValue);
-			System.out.println(meanSquareError);
-		}
-
+		ID3Tree id3Tree = new ID3Tree();
+		ID3Tree.UnmodifiedDataUnmodifiedTree uDUT = id3Tree.new UnmodifiedDataUnmodifiedTree();
+		
 	}
 
 	private static Double compareResults(ArrayList<Boolean> actualClassValue,
 			ArrayList<Boolean> derivedClassValue) {
-		int countActualTrue = 0;
-		int countDerivedTrue = 0;
+		double countActualTrue = 0.0;
+		double countDerivedTrue = 0.0;
 		for (int i = 0; i < actualClassValue.size(); i++) {
 			if (actualClassValue.get(i)) {
 				countActualTrue++;
@@ -252,36 +295,11 @@ public class ID3Tree {
 				countDerivedTrue++;
 			}
 		}
+		countActualTrue/=100.00;
+		countDerivedTrue/=100.00;
 		Double msq = (double) ((countActualTrue - countDerivedTrue) * (countActualTrue - countDerivedTrue));
 		return msq;
 
-	}
-
-	private static HashSet<Integer> getAttributeList(
-			ArrayList<Boolean[]> modifiedTestData) {
-		HashSet<Integer> attributes;
-		attributes = new HashSet<>();
-		for (int i = 0; i < modifiedTestData.get(0).length - 1; i++) {
-			attributes.add(i);
-		}
-		return attributes;
-	}
-
-	private static ArrayList<Boolean[]> generateTestDataLists(int percentage) {
-
-		ArrayList<Boolean[]> data = new ArrayList<Boolean[]>();
-		int size = testData.size();
-		int newSize = (int) ((size * percentage) / 100);
-		// System.out.println("for 50% test data full data" + testData.size()+
-		// newSize);
-
-		for (int i = 0; i < newSize; i++) {
-			Boolean[] record = testData.get(i);
-			// System.out.println("Building half tree" +
-			// Arrays.toString(record));
-			data.add(record);
-		}
-		return data;
 	}
 
 	private static void writeToFile(StringBuilder toBeWritten, String filename) {
